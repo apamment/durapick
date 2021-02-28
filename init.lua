28c560a626894eded7fea89c3a20e396c23bf2ae
Andrew Pamment <apamment@yandex.com>
2021-02-28 13:33:31 +1000

    modified...

--- init.lua
+++ init.lua
@@ -2,12 +2,12 @@
 -- Settings
 
 -- Durabilites
-durapick_durability_wood = 100 -- default:pick_wood 30
-durapick_durability_stone = 275 -- default:pick_stone 60
-durapick_durability_steel = 450 -- default:pick_steel 180
-durapick_durability_bronze = 450 -- default:pick_bronze 180
-durapick_durability_mese = 1200 -- default:pick_mese 540
-durapick_durability_diamond = 2500 -- default:pick_diamond 610
+durapick_durability_wood = 270 -- default:pick_wood 30
+durapick_durability_stone = 540 -- default:pick_stone 60
+durapick_durability_steel = 1620 -- default:pick_steel 180
+durapick_durability_bronze = 1620 -- default:pick_bronze 180
+durapick_durability_mese = 4860 -- default:pick_mese 540
+durapick_durability_diamond = 5490 -- default:pick_diamond 610
 
 -- Resources
 durapick_stick = "default:stick"
@@ -15,7 +15,7 @@ durapick_resource_steel = "default:steelblock"
 durapick_resource_bronze = "default:bronzeblock"
 durapick_resource_diamond = "default:diamondblock"
 durapick_resource_mese = "default:mese"
-durapick_resource_stone = "default:stonebrick"
+durapick_resource_stone = "moreblocks:cobble_compressed"
 durapick_resource_wood_post = "durapick:dura_wood"
 durapick_resource_wood_pre = "group:wood"
 
@@ -28,4 +28,3 @@ dofile(minetest.get_modpath("durapick") .. "/bronze.lua")
 dofile(minetest.get_modpath("durapick") .. "/steel.lua")
 dofile(minetest.get_modpath("durapick") .. "/mese.lua")
 dofile(minetest.get_modpath("durapick") .. "/diamond.lua")
-dofile(minetest.get_modpath("durapick") .. "/dev.lua")
